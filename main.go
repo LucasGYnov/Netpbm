226881188a3c583aa2e4d69b323640f4008ff3d4
Your Name <you@example.com>
2024-01-09 10:19:20 +0100

    Vérification P1 et P4 format

--- main.go
+++ main.go
@@ -29,52 +29,63 @@ func ReadPBM(filename string) (*PBM, error) {
 	}
 
 	var lines []string
+
 	for _, line := range strings.Split(string(content), "\n") {
 		if !strings.HasPrefix(line, "#") {
 			lines = append(lines, line)
 		}
 	}
 
-	magicNumber := lines[0]
+	if strings.HasPrefix(lines[0], "P1") || strings.HasPrefix(lines[0], "P4") {
+		magicNumber := lines[0]
 
-	elmtInLine := strings.Fields(lines[1]) //sépare en différents éléments la string lorsqu'un espace est rencontré
-	width, err := strconv.Atoi(elmtInLine[0])
-	height, err := strconv.Atoi(elmtInLine[1])
-
-	pbm := &PBM{
-		data:        make([][]bool, height),
-		width:       width,
-		height:      height,
-		magicNumber: magicNumber,
-	}
-
-	for i := 0; i < height; i++ {
-		bitImg := strings.Fields(lines[i+2])
-		pbm.data[i] = make([]bool, width)
-		for j, val := range bitImg {
-			bit, err := strconv.Atoi(val)
-			if err != nil {
-				return nil, fmt.Errorf("Erreur lors de la conversion: %v", err)
-			}
-			pbm.data[i][j] = bit == 1
+		elmtInLine := strings.Fields(lines[1]) //sépare en différents éléments la string lorsqu'un espace est rencontré
+		width, err := strconv.Atoi(elmtInLine[0])
+		if err != nil {
+			return nil, err
 		}
-	}
+		height, err := strconv.Atoi(elmtInLine[1])
+		if err != nil {
+			return nil, err
+		}
 
-	fmt.Println("Data:")
-	for _, row := range pbm.data {
-		for _, value := range row {
-			if value {
-				fmt.Print("1 ")
-			} else {
-				fmt.Print("0 ")
+		pbm := &PBM{
+			data:        make([][]bool, height),
+			width:       width,
+			height:      height,
+			magicNumber: magicNumber,
+		}
+
+		for i := 0; i < height; i++ {
+			bitImg := strings.Fields(lines[i+2])
+			pbm.data[i] = make([]bool, width)
+			for j, val := range bitImg {
+				bit, err := strconv.Atoi(val)
+				if err != nil {
+					return nil, fmt.Errorf("Erreur lors de la conversion: %v", err)
+				}
+				pbm.data[i][j] = bit == 1
 			}
 		}
-		fmt.Println()
+
+		fmt.Println("Data:")
+		for _, row := range pbm.data {
+			for _, value := range row {
+				if value {
+					fmt.Print("1 ")
+				} else {
+					fmt.Print("0 ")
+				}
+			}
+			fmt.Println()
+		}
+
+		fmt.Printf("Width: %d, ", pbm.width)
+		fmt.Printf("Height: %d\n", pbm.height)
+		fmt.Printf("Magic Number: %s\n", pbm.magicNumber)
+
+		return pbm, nil
+	} else {
+		return nil, fmt.Errorf("Le format de l'image n'est pas PBM.")
 	}
-
-	fmt.Printf("Width: %d, ", pbm.width)
-	fmt.Printf("Height: %d\n", pbm.height)
-	fmt.Printf("Magic Number: %s\n", pbm.magicNumber)
-
-	return pbm, nil
 }
