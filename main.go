2d0bc1079c98638a421eafc98bfa228a53bfdf4e
Your Name <you@example.com>
2024-01-09 09:46:13 +0100

    Affichage de la struct

--- main.go
+++ main.go
@@ -2,51 +2,79 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type PBM struct {
-	data          [][]bool // position en x et y, (bool => 0 ou 1 - blanc ou noir)
-	width, height int      // taille de l'image
-	magicNumber   string   //pX
+	data          [][]bool // différentes valeurs possibles (0 ou 1 - noir ou blanc => true ou false)
+	width, height int      // taille de l'image (largeur et hauteur)
+	magicNumber   string   // Px (de 1 à 4, spécifique au format PBM)
 }
 
 func main() {
-	ReadPBM("test.pbm")
+	var file string
+
+	fmt.Print("Entrer le nom du document (attention à ne pas faire d'erreur) : ")
+	fmt.Scanf("%s", &file)
+
+	ReadPBM(file)
 }
 
-func ReadPBM(filename string) /*  (*PBM, error) */ {
-	image, err := lireFichier(filename)
+func ReadPBM(filename string) (*PBM, error) {
+	content, err := os.ReadFile(filename)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	line := strings.Split(image, "\n")
-
-	var allLine []string
-
-	for _, elmt := range line {
-		entireImage := fmt.Sprintf("%s\n", elmt)
-		allLine = append(allLine, entireImage)
+	var lines []string
+	for _, line := range strings.Split(string(content), "\n") {
+		if !strings.HasPrefix(line, "#") {
+			lines = append(lines, line)
+		}
 	}
 
-	/* for i := 0; i < len(allLine); i++ {
-		fmt.Println(line[i])
-	} */
-	/* fmt.Println("Le magic number est : ", line[0], ".L'image est de largeur", line[2][0:2], "et d'hauteur", line[2][3:5], ".") */
-	magicNumber := line[0][0:2]
-	largeur := line[2][0:2]
-	hauteur := line[2][3:5]
+	magicNumber := lines[0]
 
-	fmt.Println(magicNumber, largeur, hauteur)
-}
+	elmtInLine := strings.Fields(lines[1]) //sépare en différents éléments la string lorsqu'un espace est rencontré
+	width, err := strconv.Atoi(elmtInLine[0])
+	height, err := strconv.Atoi(elmtInLine[1])
 
-func lireFichier(fichier string) (string, error) {
-	contenu, err := os.ReadFile(fichier)
-	if err != nil {
-		return "", err
+	pbm := &PBM{
+		data:        make([][]bool, height),
+		width:       width,
+		height:      height,
+		magicNumber: magicNumber,
 	}
-	return string(contenu), nil
+
+	for i := 0; i < height; i++ {
+		bitImg := strings.Fields(lines[i+2])
+		pbm.data[i] = make([]bool, width)
+		for j, val := range bitImg {
+			bit, err := strconv.Atoi(val)
+			if err != nil {
+				return nil, fmt.Errorf("Erreur lors de la conversion: %v", err)
+			}
+			pbm.data[i][j] = bit == 1
+		}
+	}
+
+	fmt.Println("Data:")
+	for _, row := range pbm.data {
+		for _, value := range row {
+			if value {
+				fmt.Print("1 ")
+			} else {
+				fmt.Print("0 ")
+			}
+		}
+		fmt.Println()
+	}
+
+	fmt.Printf("Width: %d, ", pbm.width)
+	fmt.Printf("Height: %d\n", pbm.height)
+	fmt.Printf("Magic Number: %s\n", pbm.magicNumber)
+
+	return pbm, nil
 }
